26001f5f6544068f1ad16db0ae0327f7e207b4fd
Hendra Dwi Saputra <hendra@usa.com>
2022-03-26 21:22:23 +0700

    Menambahkan bulma css dan lainnya

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { svelteTemplateEngine } from './svelte-templatee-engine';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.engine('svelte', svelteTemplateEngine);
   app.setViewEngine('svelte');
 
