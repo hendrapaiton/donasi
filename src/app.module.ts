3d7d1ff2f9881a0df9c2ea67b54755adafc4f7b3
Hendra Dwi Saputra <hendra@usa.com>
2022-03-26 16:36:33 +0700

    Membuat dua modul (index & report)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IndexModule } from './index/index.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
+  imports: [IndexModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
