bb164a4d8ffe3eec823033fc488b8d1ff8739373
Hendra Dwi Saputra <hendra@usa.com>
2022-03-26 16:53:16 +0700

    Kode dibuat lebih modular sesuai peruntukkannya

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { IndexModule } from './index/index.module';
 import { ReportModule } from './report/report.module';
 
 @Module({
   imports: [IndexModule, ReportModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/index/index.module.ts
+++ src/index/index.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { IndexController } from './index.controller';
+import { IndexService } from './index.service';
 
-@Module({})
+@Module({
+  controllers: [IndexController],
+  providers: [IndexService],
+})
 export class IndexModule {}
