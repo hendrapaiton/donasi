643a550b4753cf442e5a6b9c98157333902ad9cb
Hendra Dwi Saputra <hendra@usa.com>
2022-03-27 20:45:06 +0700

    Draft formulir pendaftaran donasi

--- views/Index.svelte
+++ views/Index.svelte
@@ -12,13 +12,34 @@
       <span class="column">
         <div class="field">
           <label class="label" for="tanggal">Jadwal Pelaksanaan</label>
-          <div class="control is-expanded">
+          <div class="control has-icons-left">
             <input
-              class="input is-fullwidth"
+              class="input"
               type="text"
               placeholder="Tanggal donasi yang dipilih"
               id="tanggal"
             />
+            <span class="icon is-small is-left">
+              <i class="fas fa-calendar" />
+            </span>
+          </div>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-danger is-light">
+              <span class="icon">
+                <i class="fa-solid fa-arrows-rotate" />
+              </span>
+              <span>Reset</span>
+            </button>
+          </div>
+          <div class="control is-expanded">
+            <button class="button is-primary is-fullwidth">
+              <span class="icon">
+                <i class="fas fa-floppy-disk" />
+              </span>
+              <span>Simpan Jadwal</span>
+            </button>
           </div>
         </div>
       </span>
