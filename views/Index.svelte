9c0e8102ff2a1cd6c8f28e7480969d66a0aa4c80
Hendra Dwi Saputra <hendra@usa.com>
2022-03-27 22:26:48 +0700

    Desain Formulir Donasi (fix) #1

--- views/Index.svelte
+++ views/Index.svelte
@@ -5,44 +5,112 @@
 </script>
 
 <Layout>
-  <div class="container">
-    <h1 class="is-size-3 has-text-centered my-4">{identitas}</h1>
-    <form class="columns">
-      <span class="column" />
-      <span class="column">
-        <div class="field">
-          <label class="label" for="tanggal">Jadwal Pelaksanaan</label>
-          <div class="control has-icons-left">
-            <input
-              class="input"
-              type="text"
-              placeholder="Tanggal donasi yang dipilih"
-              id="tanggal"
-            />
-            <span class="icon is-small is-left">
-              <i class="fas fa-calendar" />
-            </span>
-          </div>
-        </div>
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button is-danger is-light">
-              <span class="icon">
-                <i class="fa-solid fa-arrows-rotate" />
-              </span>
-              <span>Reset</span>
-            </button>
-          </div>
-          <div class="control is-expanded">
-            <button class="button is-primary is-fullwidth">
-              <span class="icon">
-                <i class="fas fa-floppy-disk" />
-              </span>
-              <span>Simpan Jadwal</span>
-            </button>
-          </div>
-        </div>
-      </span>
-    </form>
-  </div>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title has-text-centered">{identitas}</h1>
+        <form class="columns">
+          <span class="column">
+            <div class="field">
+              <label class="label" for="tanggal">Nama Lengkap</label>
+              <div class="control has-icons-left">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Nama donatur yang akan ditampilkan"
+                  id="tanggal"
+                />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-user" />
+                </span>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label" for="tanggal">Tempat Tinggal</label>
+              <div class="control has-icons-left">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Alamat rumah atau domisili"
+                  id="tanggal"
+                />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-house" />
+                </span>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label" for="tanggal">Nomor Handphone</label>
+              <div class="control has-icons-left">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Kontak yang bisa dihubungi"
+                  id="tanggal"
+                />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-phone" />
+                </span>
+              </div>
+            </div>
+          </span>
+          <span class="column">
+            <div class="field">
+              <label class="label" for="tanggal">Jadwal Pelaksanaan</label>
+              <div class="control has-icons-left">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Tanggal donasi yang dipilih"
+                  id="tanggal"
+                />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-calendar" />
+                </span>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label" for="donasi">Pilihan Donasi</label>
+              <div class="control">
+                <label class="radio">
+                  <input type="radio" />
+                  Takjil
+                </label>
+                <label class="radio">
+                  <input type="radio" />
+                  Bukber
+                </label>
+              </div>
+            </div>
+            <div class="field mt-5">
+              <div class="control">
+                <label class="checkbox">
+                  <input type="checkbox" />
+                  Simpan dengan nama "<b>Hamba Allah</b>"
+                </label>
+              </div>
+            </div>
+            <div class="field is-grouped">
+              <div class="control">
+                <button class="button is-danger is-light is-normal">
+                  <span class="icon">
+                    <i class="fa-solid fa-arrows-rotate" />
+                  </span>
+                  <span>Reset</span>
+                </button>
+              </div>
+              <div class="control is-expanded">
+                <button class="button is-primary is-fullwidth is-normal">
+                  <span class="icon">
+                    <i class="fas fa-floppy-disk" />
+                  </span>
+                  <span>Tetapkan Jadwal</span>
+                </button>
+              </div>
+            </div>
+          </span>
+        </form>
+      </div>
+    </div>
+  </section>
 </Layout>
