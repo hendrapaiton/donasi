091060c54082246c673a2d72d957472d1d9bc0af
Hendra Dwi Saputra <hendra@usa.com>
2022-03-30 19:52:49 +0700

    Menambahkan login app di proyek

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { IndexModule } from './index/index.module';
 import { ReportModule } from './report/report.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [IndexModule, ReportModule],
+  imports: [IndexModule, ReportModule, LoginModule],
 })
 export class AppModule {}
