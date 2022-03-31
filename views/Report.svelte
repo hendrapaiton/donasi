9c848c3d380d4a082b7ade440fd8e0d0b80f3adb
Hendra Dwi Saputra <hendra@usa.com>
2022-03-31 08:40:58 +0700

    Draft antarmuka report (not yet)

--- views/Report.svelte
+++ views/Report.svelte
@@ -1,11 +1,20 @@
 <script>
   import Layout from './Layout.svelte';
-
-  export let page;
 </script>
 
 <Layout>
   <div class="container">
-    <h1><i class="fas fa-print" /> {page}</h1>
+    <div class="columns">
+      <div class="column">
+        <div class="tabs is-centered">
+          <ul>
+            <li class="is-active"><a href="/#">Donasi</a></li>
+            <li><a href="/#">Jadwal</a></li>
+            <li><a href="/#">Paket</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="column">Form</div>
+    </div>
   </div>
 </Layout>
