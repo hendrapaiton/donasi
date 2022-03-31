c8e522b8b099140dba25e0e2f3c71f3609439576
Hendra Dwi Saputra <hendra@usa.com>
2022-03-31 10:49:16 +0700

    Ganti lagi karna svelte tidak jalan skripnya

--- src/svelte-templatee-engine.ts
+++
@@ -1,7 +0,0 @@
-import 'svelte/register';
-
-export function svelteTemplateEngine(filepath: string, options: any, next) {
-  const Component = require(filepath).default;
-  const { html } = Component.render(options);
-  next(null, html);
-}
